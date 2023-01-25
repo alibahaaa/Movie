8ede5cb1b72c797afd27f09c10982660a774f511
Baha <ali.bahaabadi@basalam.com>
2023-01-25 17:38:16 +0330

    [feat] add movie list entity.

---
+++ data/movie-remote/src/main/java/ir/baha/movieremote/response/MovieListResponseEntity.kt
@@ -0,0 +1,22 @@
+package ir.baha.movieremote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieListResponseEntity(
+    @SerializedName("page")
+    val page: Int,
+
+    @SerializedName("results")
+    val results: List<MovieResponseEntity>
+)
+
+data class MovieResponseEntity(
+    @SerializedName("page")
+    val id: Int,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("backdrop_path")
+    val image: String
+)
