9d73394f7681f850c68f58e0b15c46b0109e58b0
Baha <ali.bahaabadi@basalam.com>
2023-01-25 17:52:38 +0330

    [feat] add movie detail entity.

---
+++ data/movie-remote/src/main/java/ir/baha/movieremote/response/MovieDetailResponseEntity.kt
@@ -0,0 +1,19 @@
+package ir.baha.movieremote.response
+
+data class MovieDetailResponseEntity(
+    val backdrop_path: String,
+    val genres: List<Genre>,
+    val id: Int,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val poster_path: String,
+    val release_date: String,
+    val vote_average: Double,
+    val vote_count: Int
+)
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
