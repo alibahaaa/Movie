a6b562bfe0f5d1ce912e7fd1df079505e9a63e41
Baha <ali.bahaabadi@basalam.com>
2023-01-26 19:26:43 +0330

    [feat] create movie list vm.

---
+++ feature/movie-list/src/main/java/ir/baha/movielist/vm/MovieListViewModel.kt
@@ -0,0 +1,51 @@
+package ir.baha.movielist.vm
+
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.baha.designsystem.base.BaseViewModel
+import ir.baha.moviedomain.entity.MovieEntity
+import ir.baha.moviedomain.entity.Resource
+import ir.baha.moviedomain.usecase.GetMovieListUseCase
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieListViewModel @Inject constructor(
+    val getMovieListUseCase: GetMovieListUseCase
+) : BaseViewModel<MovieListIntent, MovieListAction, MovieListState>() {
+
+    override fun createInitialState(): MovieListState = MovieListState.Idle
+
+    override fun handleIntent(intent: MovieListIntent): MovieListAction = when (intent) {
+        MovieListIntent.GetMovieList -> MovieListAction.MovieList
+    }
+
+    override fun handleAction(action: MovieListAction) {
+        when (action) {
+            MovieListAction.MovieList -> viewModelScope.launch {
+                when (val response = getMovieListUseCase()) {
+                    is Resource.Error -> setState { MovieListState.Error(response.exception?.message) }
+                    is Resource.Loading -> setState { MovieListState.Loading }
+                    is Resource.Success -> setState { MovieListState.ShowMovieList(response.data!!) }
+                }
+            }
+        }
+    }
+
+}
+
+
+sealed class MovieListIntent {
+    object GetMovieList : MovieListIntent()
+}
+
+sealed class MovieListAction {
+    object MovieList : MovieListAction()
+}
+
+sealed class MovieListState {
+    object Idle : MovieListState()
+    object Loading : MovieListState()
+    data class ShowMovieList(val movies: List<MovieEntity>) : MovieListState()
+    data class Error(val error: String?) : MovieListState()
+}
