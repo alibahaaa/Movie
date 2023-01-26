442395ec3644b0e6c9935e073c64227cac80bf24
Baha <ali.bahaabadi@basalam.com>
2023-01-26 19:25:09 +0330

    [feat] make MovieEntity parcelized.

--- domain/movie-domain/src/main/java/ir/baha/moviedomain/entity/MovieEntity.kt
+++ domain/movie-domain/src/main/java/ir/baha/moviedomain/entity/MovieEntity.kt
@@ -1,7 +1,11 @@
 package ir.baha.moviedomain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class MovieEntity(
     val id: Int,
     val title: String,
     val image: String
-)
+) : Parcelable
