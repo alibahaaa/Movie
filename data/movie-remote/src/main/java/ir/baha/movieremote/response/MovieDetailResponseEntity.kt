c19b8422104c331b46ea01994c0efdb62753cbbb
Baha <ali.bahaabadi@basalam.com>
2023-01-26 14:12:43 +0330

    [feat] make MovieDetailResponseEntity serialized.

--- data/movie-remote/src/main/java/ir/baha/movieremote/response/MovieDetailResponseEntity.kt
+++ data/movie-remote/src/main/java/ir/baha/movieremote/response/MovieDetailResponseEntity.kt
@@ -1,19 +1,43 @@
 package ir.baha.movieremote.response
 
+import com.google.gson.annotations.SerializedName
+
 data class MovieDetailResponseEntity(
+    @SerializedName("backdrop_path")
     val backdrop_path: String,
-    val genres: List<Genre>,
+
+    @SerializedName("genres")
+    val genres: List<GenreResponse>,
+
+    @SerializedName("id")
     val id: Int,
+
+    @SerializedName("original_language")
     val original_language: String,
+
+    @SerializedName("original_title")
     val original_title: String,
+
+    @SerializedName("overview")
     val overview: String,
+
+    @SerializedName("poster_path")
     val poster_path: String,
+
+    @SerializedName("release_date")
     val release_date: String,
+
+    @SerializedName("vote_average")
     val vote_average: Double,
+
+    @SerializedName("vote_count")
     val vote_count: Int
 )
 
-data class Genre(
+data class GenreResponse(
+    @SerializedName("id")
     val id: Int,
+
+    @SerializedName("name")
     val name: String
 )
