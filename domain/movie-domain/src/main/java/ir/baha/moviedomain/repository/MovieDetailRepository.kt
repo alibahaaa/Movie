e7ce89b379b820f3acbeea95d7c3f5e62f7f9054
Baha <ali.bahaabadi@basalam.com>
2023-01-26 14:15:03 +0330

    [feat] add MovieDetailRepository.

---
+++ domain/movie-domain/src/main/java/ir/baha/moviedomain/repository/MovieDetailRepository.kt
@@ -0,0 +1,10 @@
+package ir.baha.moviedomain.repository
+
+import ir.baha.moviedomain.entity.MovieDetailEntity
+import ir.baha.moviedomain.entity.Resource
+
+interface MovieDetailRepository {
+
+    suspend fun getMovieDetail(): Resource<MovieDetailEntity>
+
+}
