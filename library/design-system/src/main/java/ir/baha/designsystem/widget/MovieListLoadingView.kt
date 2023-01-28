66c822e440e05aa7e60a4c6815401fb61e684079
Baha <ali.bahaabadi@basalam.com>
2023-01-28 16:36:11 +0330

    [feat] change loading ui.

--- library/design-system/src/main/java/ir/baha/designsystem/widget/MovieListLoadingView.kt
+++ library/design-system/src/main/java/ir/baha/designsystem/widget/MovieListLoadingView.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
@@ -17,12 +19,14 @@ fun MovieListLoadingView() {
         modifier = Modifier
             .testTag("loading_tag")
             .height(200.dp)
-            .shimmer(),
+            .shimmer()
+            .clip(RoundedCornerShape(4.dp)),
     ) {
         Box(
             modifier = Modifier
                 .size(200.dp)
-                .background(Color.DarkGray),
+                .background(Color.DarkGray)
+                .clip(RoundedCornerShape(4.dp)),
         )
     }
 }
